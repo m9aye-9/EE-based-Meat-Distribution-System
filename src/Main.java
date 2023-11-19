fefb1061e5bdce8724416521ea1b0a627fa80f3f
faiza <faizajamshaid69@gmail.com>
2023-11-19 21:00:40 +0500

    Added Beef,Mutton,CamelMeat classes used inheritance and used generic (nested parametrized classes)

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 
 
-        House<Mutton> house1=new House<>("house 1","abdul basheer",new Mutton(78.0),true);
+        House<Mutton> house1=new House<>("house 1","abdul basheer",new Mutton(77.0),true);
         House<Beef> house2=new House<Beef>("house 2","abdul sattar",new Beef(0.0),false);
         House<Mutton> house3=new House<Mutton>("house 3","abdul majeed",new Mutton(0.0),false);
         House<Beef> house4=new House<Beef>("house 4","abdul taha",new Beef(34.0),true);
